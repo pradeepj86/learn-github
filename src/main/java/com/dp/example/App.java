932b78f7192bba9bb2b0285f5bf615f840ca1859
pradeepj86 <pradeep.j86@gmail.com>
2016-11-20 15:52:04 +0530

    added everyone.

--- src/main/java/com/dp/example/App.java
+++ src/main/java/com/dp/example/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hellooooo !" );
+        System.out.println( "Hellooooo everyone !" );
     }
 }
